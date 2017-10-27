6a317ad6e47066f2b2d988ecfe133e9d38b5e76b
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-27 09:00:47 -0700

    add two animals to the masterAnimalList in root component

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,6 +10,7 @@ import { ZooAnimal } from './zoo-animal.model';
 
 export class AppComponent {
   masterAnimalList: ZooAnimal[] = [
-    
+    new ZooAnimal("Arctic Fox", "Moon", 2, "Carnivore", "Northen Trail", 5, "Female", "Cool shade", "Loud noises"),
+    new ZooAnimal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the sunshine", "Toys that are not rope-based"),
   ]
 }
