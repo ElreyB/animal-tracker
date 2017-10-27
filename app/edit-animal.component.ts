4b9efe285725bb84c2ae683b45e096bf61a619b0
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-27 16:26:04 -0700

    style edit animal panel

--- app/edit-animal.component.ts
+++ app/edit-animal.component.ts
@@ -4,7 +4,28 @@ import { ZooAnimal } from './zoo-animal.model';
 @Component({
   selector: 'edit-animal',
   templateUrl: 'app/html/edit-animal.component.html',
-  styles: [``]
+  styles: [`
+    p {
+      padding: 2px 16px;
+      background-color: white;
+      border-radius: 4px;
+      border: 2px solid grey;
+    }
+    h3, h2 {
+      text-align: center;
+    }
+    div.line{
+      background-color: #333333;
+      width: 515px;
+      height: 3px;
+      margin-top: 9px;
+      margin-bottom: 9px;
+    }
+    button {
+      margin-top: 9px;
+      margin-bottom: -8px;
+    }
+    `]
 })
 export class EditAnimalComponent {
   @Input() animalSelected: ZooAnimal;
